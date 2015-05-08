492093a543131a55a809fd93c0fdad0e96806bf1
PM <PM@ITBANK22>
2015-05-08 21:36:34 +0900

    몰라...

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		for (int i = 2; i < 9; i += 4) {
 			for (int j = 1; j < 9; j++) {
 				for (int k = i; k < i +4; k++) {
+					System.out.println("sdf");
 					System.out.print(k + "x" + j + "=" + (k * j) + "\t");
 				}
 				System.out.println();
