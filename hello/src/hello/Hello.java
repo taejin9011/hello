73e291abffbe3e9469ce672f8c57273e57c11587
PM <PM@ITBANK22>
2015-05-08 19:44:32 +0900

    launching..

---
+++ hello/src/hello/Hello.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Hello {
+	public static void main(String[] args) {
+		for (int i = 2; i < 9; i += 4) {
+			for (int j = 1; j < 9; j++) {
+				for (int k = i; k < i +4; k++) {
+					System.out.print(k + "x" + j + "=" + (k * j) + "\t");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
+}
